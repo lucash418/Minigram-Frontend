633fbb0fa32c65316785620d7f40dfc7a3da2371
lucash418 <lucashernando418@gmail.com>
2023-04-10 17:11:07 +0530

    sidebar file created

--- components/sidebar.js
+++ components/sidebar.js
@@ -4,39 +4,50 @@ import SearchIcon from '@mui/icons-material/Search';
 import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
-const sidebar = () => {
+import Link from 'next/link';
+export const Sidebar = () => {
   return (
     <div>
     <div className='menu'>
    <div className='menu-link'> Menu</div>
     </div>
-      <div className='Home'>
+      <div className='side-contain'>
+      <Link href="/pages/home">
      <div className='home-icon'><HomeIcon/></div> 
      <div  className='home-link'>Home</div> 
+     </Link>
       </div>
     
-    <div  className='Search'>
-      <div className='search-icon'><SearchIcon/></div>
+    <div  className='side-contain'>
+    <Link href='/pages/search'>
+    <div className='search-icon'><SearchIcon/></div>
      <div className='search-link'> Search</div>
-      </div>
-    
-    <div className='Explore'>
-   <div className='explore-icon'><ExploreOutlinedIcon/></div> 
-   <div className='explore-link'>Explore</div>
+    </Link>
       
       </div>
     
-    <div className='Message'>
-   <div className='message-icon'><TextsmsOutlinedIcon/></div> 
-   <div className='message-link'>Message</div>
+    <div className='side-contain'>
+    <Link href='/pages/explore'>
+    <div className='explore-icon'><ExploreOutlinedIcon/></div> 
+   <div className='explore-link'>Explore</div>
+    </Link>
+      
       </div>
     
-    <div className='Profile'>
-   <div className='profile-icon'><AccountCircleOutlinedIcon/></div> 
+    <div className='side-contain'>
+    <Link href='/pages/messages'>
+    <div className='message-icon'><TextsmsOutlinedIcon/></div> 
+   <div className='message-link'>Message</div>
+    </Link>
+      </div>
+    
+    <div className='side-contain'>
+    <Link href='/pages/profile'>
+    <div className='profile-icon'><AccountCircleOutlinedIcon/></div> 
    <div className='profile-link'>Profile</div>
+    </Link>
+   
       </div>
     </div>
   )
   }
-
-export default sidebar
