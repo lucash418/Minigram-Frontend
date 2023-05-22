9333b0ebfb01f6e04523acfecd6427532b53db15
lucash418 <lucashernando418@gmail.com>
2023-05-22 18:13:25 +0530

    modified home page layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 
-import React from 'react'
-import {Cards} from '../components/Cards';
-import {Sidebar} from '../components/Sidebar.js'
-import {Messages} from '../components/Messages.js'
+import React,{useState} from 'react'
+import { Cards } from '../components/Cards';
+import { Sidebar } from '../components/Sidebar.js'
+import { Messages } from '../components/Messages.js'
 import styles from '../styles/Home.module.css';
 
-export default function index(){
+export default function index() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className={styles.hello}>
-      <Sidebar/>
-      <Cards/>
-      <Messages/>
-    </div>
+      <div className={styles.homeContainer}>
+        <Sidebar />
+        <Cards/>
+        <Messages/>
+      </div>
   )
 }
 
