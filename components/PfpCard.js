f03b5f4a0f3e65c003e82a592f04594cb9c0d4a4
lucash418 <lucashernando418@gmail.com>
2023-06-09 18:41:33 +0530

    modified the cards

--- components/PfpCard.js
+++ components/PfpCard.js
@@ -14,20 +14,19 @@ export const PfpCard = (props) => {
     const user = JSON.parse(localStorage.getItem("user_info"));
     // console.log(user);
     fetchPost(user.result._id).then((resp) => {
-      setPost(resp.data);
+      setPost(resp.data);             
     });
   });
 
   let p = [];
-  // if(posts!=null)
-  //     p=posts;
-  //     console.log(p);
-  p = [
-    "https://i.pinimg.com/originals/12/04/94/120494876f3572f1ecb1f722fcc6e8b0.png",
-    "https://i.pinimg.com/736x/d2/8c/7c/d28c7c485b4a6a081610e0adaa8d5789--tree-wallpaper-wallpaper-desktop.jpg",
-    "https://i.pinimg.com/originals/97/76/ea/9776ea3c44ede2ba53f297c4c99b70a5.jpg",
-    "https://i.scdn.co/image/ab67706c0000da84bfb952f89350d7c8d6fae332",
-  ];
+  if (posts != null) p = posts;
+  console.log(p);
+  // p = [
+  //   "https://i.pinimg.com/originals/12/04/94/120494876f3572f1ecb1f722fcc6e8b0.png",
+  //   "https://i.pinimg.com/736x/d2/8c/7c/d28c7c485b4a6a081610e0adaa8d5789--tree-wallpaper-wallpaper-desktop.jpg",
+  //   "https://i.pinimg.com/originals/97/76/ea/9776ea3c44ede2ba53f297c4c99b70a5.jpg",
+  //   "https://i.scdn.co/image/ab67706c0000da84bfb952f89350d7c8d6fae332",
+  // ];
   return (
     <div className={styles.grid}>
       {p.map((user) => (
